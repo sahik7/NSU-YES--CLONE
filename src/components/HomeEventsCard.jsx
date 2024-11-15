a105bc83ea8a6398ed11eedfb9a8cbb1a159d4ee
Sahik Ahsan <sahikahsan7@gmail.com>
2024-11-15 10:14:53 +0600

    image for events

--- src/components/HomeEventsCard.jsx
+++ src/components/HomeEventsCard.jsx
@@ -3,23 +3,24 @@ import React from 'react';
 function HomeEventsCard({cardItem}) {
     const {img, title,description,highlight} = cardItem
   return (
-    <div className='rounded-[15px] overflow-hidden shadow-lg my-10'>
-      <img
-        className="w-full h-auto rounded-se-[15px]"
-        src={img}
-        alt="Admaker Bangladesh"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-2xl text-[#09529e] mb-2">{title}</div>
-        <p className="text-gray-700 text-base">
-          <b>{highlight}</b>
-          {description}
-        </p>
-        <button className='text-sm rounded mt-5 py-2 px-5 border border-gray-600 hover:border-[#218ced] hover:text-[#218ced]'>
-          See More
-        </button>
-      </div>
-    </div>
+    <div className="rounded-[15px] overflow-hidden shadow-lg my-10">
+  <div className="overflow-hidden">
+    <img
+      className="w-full h-auto hover:scale-125 duration-700"
+      src={img}
+      alt="Admaker Bangladesh"
+    />
+  </div>
+  <div className="px-6 py-4">
+    <div className="font-bold text-2xl text-[#09529e] mb-2">{title}</div>
+    <p className="text-gray-700 text-base">
+      <b>{highlight}</b> {description}
+    </p>
+    <button className="text-sm rounded mt-5 py-2 px-5 border border-gray-600 hover:border-[#218ced] hover:text-[#218ced]">
+      See More
+    </button>
+  </div>
+</div>
   );
 }
 
